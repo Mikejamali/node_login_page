d86f9314a7a33af027cbf0146d01ed92ad1d6324
Mikejamali <michaeljamali@gmail.com>
2017-08-18 17:06:14 -0400

    fixed the bugs

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,40 @@
 const express = require("express");
 const router = express.Router();
 let user = { username: "mike", password: "mikepass"};
+let data;
 
+// function authenticate(req, res, next) {
+//   if (req.session.token) {
+//     res.redirect("/");
+//   } else {
+//     console.log("No token");
+//     next();
+//   }
+// }
 
-function authenticate(req, res, next) {
+router.get("/", function(req, res, next) {
   if (req.session.token) {
-    res.redirect("/results");
-  } else {
-    // console.log("No token");
-    next();
+    res.render("results", data)
   }
-}
-
-router.get("/", authenticate, function(req, res) {
-  console.log("hello");
-  res.render("login");
+  else {
+  // console.log("hello");
+  res.redirect("/login");
+ }
+},
+ function(req, res) {
+  res.render("home", {user: req.session.username});
 });
 
-router.post("/", function(req, res) {
+router.get("/login", function(req, res){
+  console.log(data);
+  res.render("login", data)
+});
+
+
+router.post("/login", function(req, res, next) {
   console.log("checking request" + req);
+
+
   req.checkBody("username", "username cannot contain any special characters.").isAlphanumeric();
   req.checkBody("username", "username cannot be empty.").notEmpty({min: 8});
   req.checkBody("username", "Must be a username.").isLength({max: 25});
@@ -32,41 +48,58 @@ router.post("/", function(req, res) {
       messages.push(error.msg);
     });
 
-    // console.log(messages);
+    if (messages.length > 0) {
+      res.redirect("/login");
+    }
+    else
+      data = {
+        username: req.body.username,
+        password: req.body.password,
+   };
+    if (req.body.username === user.username && req.body.password === user.password) {
+      req.session.token = "afs29628";
+       data = {
+        username: req.body.username,
+        password: req.body.password,
+      };
 
-    let data = {
-      errors: messages,
-      username: req.body.username,
-      password: req.body.Password,
+      res.redirect('/');
+    }
+    else {
+      data = {
+       errors: messages
+     };
+      res.redirect("/login")
     };
 
-    res.render('results', data);
+
+
   });
 });
 
 
-router.get("/results", function(req, res, next) {
+router.get("/home", function(req, res, next) {
   if (req.session.token) {
     next();
   } else {
     res.redirect("/")
   }
 }, function(req, res) {
-  res.render("results", req.session.user);
+    res.render("home", req.session.user);
 });
 
 
-router.post("/results", function(req, res) {
+router.post("/home", function(req, res) {
   let obj = {
     username: req.body.username,
     password: req.body.password
   };
 
-  if (obj.username == user.username && obj.password == user.password) {
+  if (obj.errors.length == 0) {
     req.session.user = obj;
     req.session.token = "afs29628";
-    res.redirect("/results");
-  } else {
+    res.redirect("/");
+  } else { ////look at else
     res.redirect("/");
   }
 });
